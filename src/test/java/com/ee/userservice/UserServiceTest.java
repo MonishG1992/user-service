a5ab5a4ab22396847d755289d5eb0e6a5ff3fb4b
MonishG <monish.gandhi@ril.com>
2019-06-21 16:37:56 +0530

    adding user service

---
+++ src/test/java/com/ee/userservice/UserServiceTest.java
@@ -0,0 +1,31 @@
+package com.ee.userservice;
+
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.SpringRestPactRunner;
+import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRestPactRunner.class)
+@Provider("user_service")
+@PactFolder("../order-service-master/target/pacts")
+//@PactBroker(host = "broker_app", port = "80", tags = {"latest", "DEV", "PROD"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserServiceTest {
+
+   @TestTarget
+   public final Target target = new SpringBootHttpTarget();
+   
+   @State("User Paresh exists")
+   public void userPareshExist() {
+	   // setup user parent
+	   System.out.println("User Paresh Exist in database");
+	   System.out.println();
+   }
+   
+}
